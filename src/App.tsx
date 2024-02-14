caf2e13b371ff521d7d03aed2aba45f75d451cac
VladKlimakhovich <klimaxovich@mail.ru>
2024-02-14 13:06:06 +0400

    modify app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ProjectsPage from './projects/ProjectsPage';
+import { BrowserRouter as Router, Routes, Route, NavLink} from 'react-router-dom';
+import HomePage from './home/HomePage';
 
 function App() {
   return (
-    <div className='container'>
-      <ProjectsPage />
-    </div>
-  );
+        <Router>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+            </Routes>
+          </div>
+        </Router>
+      );
 }
 
 export default App;
