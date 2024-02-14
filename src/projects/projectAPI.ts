70eace9196b7e96c0268d8bb2f82a2e5e1f3e808
VladKlimakhovich <klimaxovich@mail.ru>
2024-02-14 11:18:01 +0400

    Change limit of cards on site

--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -51,7 +51,7 @@ function convertToProjectModel(item: any): Project {
 }
 
 const projectAPI = {
-  get(page = 1, limit = 20) {
+  get(page = 1, limit = 8) {
     return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
       //.then(delay(600))
       .then(checkStatus)
